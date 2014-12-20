0161b9c47615387cbbdd397b62e7c9cbcfc8059f
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-12-20 19:11:41 +0900

    Setup es6_module_transpiler-rails specified Ember app

--- lib/ember/rails/engine.rb
+++ lib/ember/rails/engine.rb
@@ -1,5 +1,6 @@
 require 'ember/handlebars/template'
 require 'active_model_serializers'
+require 'es6_module_transpiler/rails'
 require 'sprockets/railtie'
 
 module Ember
@@ -17,6 +18,25 @@ module Ember
         Sprockets.register_engine '.handlebars', Ember::Handlebars::Template
         Sprockets.register_engine '.hbs', Ember::Handlebars::Template
         Sprockets.register_engine '.hjs', Ember::Handlebars::Template
+      end
+
+      config.es6 = ActiveSupport::OrderedOptions.new
+
+      config.es6.module_prefix = 'my-app'
+      config.es6.module_dir = %w(
+        models controllers views routes components helpers mixins serializers adapters
+        initializers
+        router store
+      )
+
+      config.after_initialize do |app|
+        if config.es6.module_prefix
+          Array(config.es6.module_dir).each do |dir|
+            path_pattern = Regexp.new("^#{app.root.join('app', 'assets', 'javascripts', dir)}")
+
+            ES6ModuleTranspiler.add_prefix_pattern path_pattern, config.es6.module_prefix
+          end
+        end
       end
     end
   end
