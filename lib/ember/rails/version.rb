d0c94e99d8b989f0a906e57be2f54a4b18db8585
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-12-20 21:02:31 +0900

    Fetch modules for Ember.js with es6 syntax

--- lib/ember/rails/version.rb
+++ lib/ember/rails/version.rb
@@ -1,5 +1,9 @@
 module Ember
   module Rails
     VERSION = '0.15.1'
+
+    EMBER_LOADER_VERSION = '2.1.1'
+    EMBER_RESOLVER_VERSION = '0.1.10'
+    EMBER_LOAD_INITIALIZERS_VERSION = '0.0.2'
   end
 end
